d01289eeda2dc0020beaf18b0616ffc9f575d13f
nikit <nikita.nikita@gmail.com>
2022-12-06 11:16:30 +0530

    Ability to peak and pop from the Stack till it is empty 56->30->70

--- StackAndQueue/Node.cs
+++ StackAndQueue/Node.cs
@@ -16,5 +16,4 @@ namespace StackAndQueue
             this.data = data;
         }
     }
-
 }
--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -10,14 +10,6 @@ namespace StackAndQueue
     {
         static void Main(string[] args)
         {
-            //UC1
-            Console.WriteLine("Hello Stack using Linked List!");
-            CreateStack createStatck = new CreateStack();
-            createStatck.Push(70);
-            createStatck.Push(30);
-            createStatck.Push(56);
-            createStatck.Display();
-
             //UC2
             Console.WriteLine("Hello Stack using Linked List!");
             PeakPop Stack = new PeakPop();
@@ -29,25 +21,6 @@ namespace StackAndQueue
             Stack.Pop();
             Stack.IsEmpty();
             Stack.Display();
-            Console.ReadKey();
-
-            //UC3
-            Console.WriteLine("Hello Queue using linked List!");
-            CreateQueue Queue = new CreateQueue();
-            Queue.Enqueue(56);
-            Queue.Enqueue(30);
-            Queue.Enqueue(70);
-            Queue.Display();// this.top=this.top.next
-            Console.ReadKey();
-
-            //UC4
-            Console.WriteLine("Hello Queue using Linked List!");
-            Dequeue DQueue = new Dequeue();
-            DQueue.Enqueue(56);
-            DQueue.Enqueue(30);
-            DQueue.Enqueue(70);
-            DQueue.Dequeu();
-            DQueue.Display();// this.top=this.top.next
             Console.ReadKey();
         }
     }
