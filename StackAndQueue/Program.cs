eae4eaae2e78c614ccdeff6442714ba647011279
nikit <nikita.nikita@gmail.com>
2022-12-06 11:12:34 +0530

    Add project files.

---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1
+            Console.WriteLine("Hello Stack using Linked List!");
+            CreateStack createStatck = new CreateStack();
+            createStatck.Push(70);
+            createStatck.Push(30);
+            createStatck.Push(56);
+            createStatck.Display();
+
+            //UC2
+            Console.WriteLine("Hello Stack using Linked List!");
+            PeakPop Stack = new PeakPop();
+            Stack.Push(70);
+            Stack.Push(30);
+            Stack.Push(56);
+            Stack.Display();
+            Stack.Peek();
+            Stack.Pop();
+            Stack.IsEmpty();
+            Stack.Display();
+            Console.ReadKey();
+
+            //UC3
+            Console.WriteLine("Hello Queue using linked List!");
+            CreateQueue Queue = new CreateQueue();
+            Queue.Enqueue(56);
+            Queue.Enqueue(30);
+            Queue.Enqueue(70);
+            Queue.Display();// this.top=this.top.next
+            Console.ReadKey();
+
+            //UC4
+            Console.WriteLine("Hello Queue using Linked List!");
+            Dequeue DQueue = new Dequeue();
+            DQueue.Enqueue(56);
+            DQueue.Enqueue(30);
+            DQueue.Enqueue(70);
+            DQueue.Dequeu();
+            DQueue.Display();// this.top=this.top.next
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StackAndQueue/UC1Create Stack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    class CreateStack
+    {
+        private Node top;
+        //public static int a = 10;
+        public CreateStack()
+        {
+            this.top = null;
+        }
+
+        //30
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack ", value);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " "); //56 30 70 
+                temp = temp.next;//1400
+            }
+
+        }
+    }
+    
+}
